b68be14cdca9c935758b8e034c00cc5cc084d3c9
David SANTOS <milleunium300@hotmail.com>
2022-01-15 15:50:48 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'faker'
+
+Article.destroy_all
+
+version = 1
+
+10.times do
+        article = Article.new(title: "Matrix #{version}", content: "cest la version #{version} du film Matrix")
+        version += 1
+        article.save!
+        puts article.title
+    end
+
+# Article.new(title: "Matrix", content: "trop bien")
+# Article.new(title: "You", content: "article de fou")
+# Article.new(title: "Draw", content: "connais pas")
+
+
